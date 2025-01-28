5356b745a8f4992c85127ca5bfec67491c9e5a59
SPerezdeTudela <sperezde@uci.edu>
2025-01-28 15:28:00 -0800

    error: build.gradle for carousel not working

--- app/src/main/java/com/example/autospa/WashPackageActivity.kt
+++ app/src/main/java/com/example/autospa/WashPackageActivity.kt
@@ -3,21 +3,40 @@ package com.example.autospa
 import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.helper.widget.Carousel
 
 class WashPackageActivity: AppCompatActivity() {
+
+    lateinit var carousel: Carousel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wash_package)
 
         val button = findViewById<Button>(R.id.button)
-
+        val carousel = findViewById<Carousel>(R.id.carousel)
         button.setOnClickListener{
             val intent = Intent(this@WashPackageActivity, OptionalAddOnsActivity::class.java)
             startActivity(intent)
         }
 
+        carousel.setAdapter(object: Carousel.Adapter {
+            override fun count(): Int {
+                TODO("Not yet implemented")
+            }
+
+            override fun populate(view: View?, index: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onNewItem(index: Int) {
+                TODO("Not yet implemented")
+            }
+        })
+
 
     }
+
 }
