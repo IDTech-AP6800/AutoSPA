9996c9bd49247587d6071c1fb44e7f72e9d611cb
Ngoc Huynh <97254009+ngocvbh@users.noreply.github.com>
2025-01-31 02:26:34 -0800

    feat: qr code receipt page, style: adjust the centering of receipt options

---
+++ app/src/main/java/com/example/autospa/QRCodeReceiptActivity.kt
@@ -0,0 +1,12 @@
+package com.example.autospa
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class QRCodeReceiptActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qr_code_receipt)
+    }
+}
---
+++ app/src/main/java/com/example/autospa/ReceiptOptionsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.autospa
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ReceiptOptionsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_receipt_options)
+    }
+}
