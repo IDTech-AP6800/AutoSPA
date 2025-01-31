c3bc256fa16505279c8f737b3fea54492d5f34b7
Brian <schultzrhouse@gmail.com>
2025-01-30 18:08:14 -0800

    feat: add home page animation

--- app/src/main/java/com/example/autospa/MainActivity.kt
+++ app/src/main/java/com/example/autospa/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.example.autospa
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private var hasClicked = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rootView = findViewById<View>(android.R.id.content)
-        rootView.setOnClickListener {
-            val intent = Intent(this@MainActivity, WashPackageActivity::class.java)
-            startActivity(intent)
-        }
+        val backgroundImageView = findViewById<ImageView>(R.id.background_3)
 
+        // Load the translate animation
+        val translateAnimation: Animation = AnimationUtils.loadAnimation(this, R.anim.translate_background)
+
+        // Apply the animation to the background_1 ImageView
+        backgroundImageView.startAnimation(translateAnimation)
+
+        // Listen for the user's click event
+        rootView.setOnClickListener {
+            if (!hasClicked) {
+                // Prevent the animation from playing multiple times
+                hasClicked = true
+
+                // Start the next activity when the user clicks
+                val intent = Intent(this@MainActivity, WashPackageActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
