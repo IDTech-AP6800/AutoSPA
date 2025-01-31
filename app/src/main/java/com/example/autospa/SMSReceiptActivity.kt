b6203847b25ff4f84a06d01c1c6998a06e573f6c
Ngoc Huynh <97254009+ngocvbh@users.noreply.github.com>
2025-01-31 09:37:58 -0800

    feat: add sms receipt page

---
+++ app/src/main/java/com/example/autospa/SMSReceiptActivity.kt
@@ -0,0 +1,12 @@
+package com.example.autospa
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SMSReceiptActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sms_receipt)
+    }
+}
