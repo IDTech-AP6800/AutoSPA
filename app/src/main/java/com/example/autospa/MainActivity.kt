f3027e0b0ebff168221a598a510ce66be6b37201
Santiago Palomares <117400795+santiagopalomares@users.noreply.github.com>
2025-02-01 14:00:13 -0800

    feat: connection to payment server

--- app/src/main/java/com/example/autospa/MainActivity.kt
+++ app/src/main/java/com/example/autospa/MainActivity.kt
@@ -2,16 +2,24 @@ package com.example.autospa
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.example.autospa.viewmodels.MainViewModel
+import com.idtech.zsdk_client.Client
+import com.idtech.zsdk_client.GetDevicesAsync
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
     private var hasClicked = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Client.GetDevicesAsync()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -20,20 +28,23 @@ class MainActivity : AppCompatActivity() {
 
         // Load the translate animation
         val translateAnimation: Animation = AnimationUtils.loadAnimation(this, R.anim.translate_background)
-
-        // Apply the animation to the background_1 ImageView
         backgroundImageView.startAnimation(translateAnimation)
 
         // Listen for the user's click event
         rootView.setOnClickListener {
             if (!hasClicked) {
-                // Prevent the animation from playing multiple times
                 hasClicked = true
-
-                // Start the next activity when the user clicks
                 val intent = Intent(this@MainActivity, WashPackageActivity::class.java)
                 startActivity(intent)
             }
         }
+
+        // Observe connection status
+        viewModel.connectionStatus.observe(this) { status ->
+            Log.d("ConnectionStatus", "Connection status: $status")
+        }
+
+        // Trigger server connection
+        viewModel.connectToServer("127.0.0.1", "42501", this)
     }
 }
